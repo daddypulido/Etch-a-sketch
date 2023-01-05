79884c592d77bfa6e6601cd7eb93b8a38dfe1895
Alex Pulido <alexanderpulido10@gmail.com>
2023-01-04 19:05:52 -0800

    created a 16x16 grid and color slider to change background color of divs in the grid

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+const container = document.querySelector('.container');
+
+
+
+function makeDiv(numDivs){
+    for(let d =0; d < numDivs; d++){
+let square = document.createElement('div');
+square.classList.add('grid');
+container.appendChild(square)
+
+}
+}
+makeDiv(256)
+
+
+
+let elements = document.getElementsByClassName("grid");
+for(let i = 0 ; i < elements.length; i++){
+    elements[i].addEventListener("mouseover",changeColor)
+   
+
+
+}
+
+
+
+function changeColor(){
+        let red = document.getElementById('red').value;
+        let green = document.getElementById('green').value;
+        let blue = document.getElementById('blue').value;
+        let color = `rgb(${red},${green},${blue})`;   
+        document.getElementById('box').value = color;
+        this.style.backgroundColor = color; 
+    console.log(color)
+}
+
 
